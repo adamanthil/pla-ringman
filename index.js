24c27792071fddce433860f347cb1c1785a31d88
Andrew Bender <andrew@bender.io>
2019-03-12 15:54:26 -0500

    Setup basic csv downloading

--- index.js
+++ index.js
@@ -1,11 +1,57 @@
+const axios = require('axios');
 const puppeteer = require('puppeteer');
+
+const bidHistoryRequestData = require('./json/bid-history-request-data.json')
+const seatingAssignmentRequestData = require('./json/seating-assignment-request-data.json')
 
 require('dotenv').config();
 
-(async () => {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
 
+async function getReportCSV(data, cookies) {
+  const cookieHeaderValue = cookies.map((cookie) => cookie.name + '=' + cookie.value + ';').join(' ')
+  const headers = {
+    Cookie: cookieHeaderValue
+  }
+
+  data.auction = process.env.AUCTION_ID
+  data.organization = process.env.ORGANIZATION_ID
+
+  const exportResponse = await axios({
+    method: 'post',
+    url: 'https://auctria.com/API/Report/Export',
+    headers: headers,
+    data: data
+  });
+
+  let progressResponse
+  do {
+    progressResponse = await axios({
+      method: 'post',
+      url: 'https://auctria.com/API/Job/Progress',
+      headers: headers,
+      data: {
+        auction: process.env.AUCTION_ID,
+        job: exportResponse.data.Id
+      }
+    });
+  } while (!progressResponse.data.Result.Complete)
+
+  const csvResponse = await axios.get(
+    `https://auctria.com/Az/${process.env.AUCTION_ID}/Job/Result`,
+    {
+      headers: headers,
+      params: {
+        job_id: exportResponse.data.Id,
+        organization: process.env.ORGANIZATION_ID
+      }
+    }
+  )
+
+  return csvResponse.data
+}
+
+
+async function logIn(page) {
   const navigationPromise = page.waitForNavigation()
 
   const loginEmailSelector = '#txtSignInEmail'
@@ -19,8 +65,21 @@ require('dotenv').config();
   await navigationPromise
 
   await page.waitForNavigation({ waitUntil: 'networkidle0' })
+  // await page.screenshot({ path: 'example.png' })
+  const cookies = await page.cookies()
+  return cookies
+}
 
-  await page.screenshot({ path: 'example.png' })
+(async () => {
+
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  const cookies = await logIn(page)
+
+  const seatingAssignmentCSV = await getReportCSV(seatingAssignmentRequestData, cookies)
+  const csv = await getReportCSV(bidHistoryRequestData, cookies)
+
+  console.log(seatingAssignmentCSV)
 
   await browser.close()
 })();
